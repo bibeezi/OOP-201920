560a883c959c8e1de06b885298a2a9e20a7c33b9
bibeezi <44236242+bibeezi@users.noreply.github.com>
2019-10-27 15:46:03 +0000

    lab4 finished

--- lab4_word_cloud.py
+++ lab4_word_cloud.py
@@ -12,7 +12,10 @@ class WordCloud:
         self.my_dict = self.create_dict()
         # you might like to run the following line only
         # if there wasn't a problem creating the dictionary
-        self.create_html(self.my_dict)
+        if self.my_dict:
+            self.create_html(self.my_dict)
+        else:
+            print("issue with input file")
 
     # this function creates the actual html file
     # takes a dictionary as an argument
@@ -20,21 +23,28 @@ class WordCloud:
     # in order to get a decent size output in the html
     def create_html(self, the_dict):
         fo = open("output.html", "w")
-        fo.write('<!DOCTYPE html>\
-            <html>\
-            <head lang="en">\
-            <meta charset="UTF-8">\
-            <title>Tag Cloud Generator</title>\
-            </head>\
-            <body>\
-            <div style="text-align: center; vertical-align: middle; font-family: arial; color: white; background-color:black; border:1px solid black">')
+        fo.write("""<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <title>Tag Cloud Generator</title>
+    </head>
+    <body>
+        <div style="text-align: center; vertical-align: middle; font-family: arial; color: white; background-color:black; border:1px solid black">
+""")
 
         # your code goes here!
-        fo.write('<span style="font-size: 10px"> HELLO </span>')
+        #fo.write('<span style="font-size: 10px"> HELLO </span>')
+        for key in the_dict.keys():
+            fo.write('            <span style="font-size: ')
+            fo.write(str(the_dict[key]*10))
+            fo.write('px"> ')
+            fo.write(key)
+            fo.write(' </span>\n')
 
-        fo.write('</div>\
-            </body>\
-            </html>')
+        fo.write("""        </div>
+    </body>
+</html>""")
 
 
     # opens the input file gettisburg.txt
@@ -48,6 +58,19 @@ class WordCloud:
     def create_dict(self):
         my_dict = {}
         # your code goes here:
+        try:
+            fo = open("gettisburg.txt","r")
+        except Exception as e:
+            my_dict = False
+            print("Caught this err0r: %s" % e.__class__.__name__)
+        else:
+            for line in fo:
+                line = line.lower()
+                line = line.split()
+                for w in line:
+                    self.add_to_dict(w, my_dict)
+
+            fo.close()
 
         return my_dict
 
@@ -60,6 +83,13 @@ class WordCloud:
     # returns a dictionary
     def add_to_dict(self, word, the_dict):
         # your code goes here
+        for key in the_dict.keys():
+            if key == word:
+                the_dict[key] = the_dict[key]+1
+                return the_dict
+        else:
+            the_dict[word] = 1
+            return the_dict
 
         return the_dict
 
